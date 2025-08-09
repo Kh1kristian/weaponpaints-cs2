cb36f8bea3f56862087d30f80a4ce69de0b483e7
kh1kristian <96524133+Kh1kristian@users.noreply.github.com>
2025-08-09 20:59:59 +0200

    Update config.php

--- class/config.php
+++ class/config.php
@@ -9,7 +9,7 @@ define('DB_PASS', '');
 
 define('WEB_STYLE_DARK', true);
 
-define('STEAM_API_KEY', '');
+define('STEAM_API_KEY', 'B846BDCD5AB8E5B8DC3FA1AEF513C523');
 define('STEAM_DOMAIN_NAME', '');
 define('STEAM_LOGOUT_PAGE', '');
 define('STEAM_LOGIN_PAGE', '');
